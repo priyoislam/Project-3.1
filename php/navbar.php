1cfe92e80a1b9925b368c5d901180b9b6603daf1
Priyo Islam <priyoislam@gmail.com>
2021-09-25 19:10:35 +0600

    nav

--- php/navbar.php
+++ php/navbar.php
@@ -7,22 +7,30 @@
     <title>Blog</title>
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/animate.css">
+    <!-- <link rel="stylesheet" href="../css/animate.css"> -->
     <link rel="stylesheet" href="../css/navbar.css">
-    <link rel="stylesheet" href="../css/footer.css">
+    
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Merriweather&family=Montserrat&family=Sacramento&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div class="header">
-        <nav class="navbar navbar-expand-lg navbar-dark nav_bg">
-            <div class="container">
-                <a class="navbar-brand" href="#">Navbar</a>
+   
+
+
+
+            <div class="navbar navbar-expand-lg navbar-dark nav_bg">
+
+
+                 <a class="navbar-brand" href="#">Navbar</a>
+
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+                
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto">
+
+                    <ul class="navbar-nav  nav-tabs ">
                         <li class="nav-item active">
                             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                         </li>
@@ -38,14 +46,18 @@
                         <li class="nav-item">
                             <a class="nav-link" href="#">Sign up</a>
                         </li>
-                        <form class="d-flex">
+                        <li>
+                        <form class="d-flex" style="padding-top:10px;">
                             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                             <button class="btn btn-outline-light btn_src" type="submit">Search</button>
                         </form>
-                </div>
+
+                        </li>
+                    </ul>
+         
+
             </div>
-        </nav>
-    </div>
+   
     <script src="../js/jquery-3.5.1.js"></script>
     <script src="../js/bootstrap.bundle.min.js"></script>
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
