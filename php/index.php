b065bdaccf9b576c6b4f7efe25dc4a4f56fdbc47
Priyo Islam <priyoislam@gmail.com>
2022-03-31 12:27:30 +0600

    update

---
+++ php/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/animate.css">
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/pboot.css">
+</head>
+
+<script>
+    $(function(){
+      var includes = $('[data-include]');
+      jQuery.each(includes, function(){
+        var file = $(this).data('include') + '.html';
+        $(this).load(file);
+      });
+    });
+</script>
+
+<body>
+<?php include("navbar.php");?>
+<?php include("footer.php");?>
+
+    <script src="../js/jquery-3.5.1.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="../js/wow.js"></script>
+    <script>
+    new WOW.init();
+    </script>
+    <script src="../js/main.js"></script>
+</body>
+
+</html>
